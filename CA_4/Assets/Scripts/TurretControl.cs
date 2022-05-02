197a49edafc247e15f51ee51e47c06cf9ebdee26
ciaramol <20089106@mail.wit.ie>
2022-05-02 23:25:46 +0100

    added turret functionality
    It doesn't do anything except shoot. Tested it in the indoor scene so I didn't interfere with anything else

---
+++ CA_4/Assets/Scripts/TurretControl.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretControl : MonoBehaviour
+{
+    Transform Player;
+    float distance;
+    public float maxDistance;
+    public Transform turretHead, spawn1, spawn2;
+    public GameObject laser;
+    public float projectileSpeed = 1000f;
+    public float fireRate, nextFire;
+    GameObject clone;
+    bool lastSpawn1 = false;
+    Vector3 spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(Player.position, transform.position);
+        if(distance <= maxDistance)
+        {
+            turretHead.LookAt(Player);
+            if(Time.time >= nextFire)
+            {
+                nextFire = Time.time + 1f / fireRate;
+                if (lastSpawn1)
+                {
+                    spawnPoint = spawn1.position;
+                }
+                else
+                {
+                    spawnPoint = spawn2.position;
+                }
+                Shoot();
+            }
+        }
+    }
+    void Shoot()
+    {
+        clone = Instantiate(laser, spawnPoint, turretHead.rotation);
+        clone.GetComponent<Rigidbody>().AddForce(turretHead.forward * projectileSpeed);
+        lastSpawn1 = !lastSpawn1;
+        Destroy(clone, 25);
+    }
+}
