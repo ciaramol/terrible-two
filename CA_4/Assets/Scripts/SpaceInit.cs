58740dcf5f7cc80a87432cc4bedef8ab178ce8f3
drophy <drophyghost@gmail.com>
2022-05-10 21:20:32 +0100

    Change vertical column distribution towards middle

--- CA_4/Assets/Scripts/SpaceInit.cs
+++ CA_4/Assets/Scripts/SpaceInit.cs
@@ -14,8 +14,13 @@ public class SpaceInit : MonoBehaviour
         Vector3 spawnCoordinates = new Vector3();
         for(int i = 0; i < columnQuantity; i++)
         {
-            spawnCoordinates.y = Random.Range(0, 1000 - columnWidth); // planet size (1000) - column size
+            // Pick Coordinates
+            // spawnCoordinates.y = Random.Range(0, 1000 - columnWidth); // planet size (1000) - column size // even distribution (prior implementation)
             spawnCoordinates.z = Random.Range(-1000 + columnWidth, 0);
+            // get more columns in middle w/o even distribution on y
+            float radius = (1000 - columnWidth) / 2;
+            spawnCoordinates.y = (Random.insideUnitCircle * radius).x + radius; // .x or .y are fine, + radius is because we only want positive values
+
             Instantiate(column, spawnCoordinates, Quaternion.identity);
             // The Quarternion.identity thing is from https://docs.unity3d.com/ScriptReference/Object.Instantiate.html
         }
